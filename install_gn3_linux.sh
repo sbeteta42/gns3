31d96c8b1144664d079579328583ac64c350d9e0
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2025-08-10 20:41:16 +0200

    Update install_gn3_linux.sh

--- install_gn3_linux.sh
+++ install_gn3_linux.sh
@@ -1,32 +1,41 @@
-# installation de GNS3
-echo " installation du dépot GNS3"
+# Installation automatisée de GNS3 pour Debian/Ubuntu/Mint
+# par sbeteta@beteta.org
+
+# Installation de GNS3
+echo "[1/7] Installation du dépot GNS3"
 sleep 5
 sudo add-apt-repository ppa:gns3/ppa -y
 
-echo " Mise à jour de votre OS... please wait..."
+# Maj de l'OS
+echo "[2/7] Mise à jour de votre OS... please wait..."
 sleep 5
 sudo apt update -y                                
 clear
 
-echo " Installation de GNS3..."
+# Installation de GNS3
+echo "[3/7] Installation de GNS3..."
 sleep 5
 sudo apt install gns3-gui gns3-server -y
 
-echo " Ajout de dépendances fonctionnelles.."
+echo "[4/7] Ajout de dépendances fonctionnelles.."
 sleep 5
 sudo dpkg --add-architecture i386 -y
 sudo apt update -y
 clear
+
+# Ajout du support IOU
+echo "[5/7] Ajout du support IOU 
+sleep 5
 sudo apt install gns3-iou -y
+
+# Installation de DOCKER
+echo "[6/7] Installation de DOCKER
+sleep 5
 sudo apt-get -y --no-install-recommends install curl apt-transport-https ca-certificates software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update -y
 clear
-sudo apt install docker docker-compose -y
-sudo usermod -aG libvirt $USER
-sudo usermod -aG kvm $USER
-sudo usermod -aG wireshark $USER
-sudo usermod -aG docker $USER
-sudo usermod -aG ubridge $USER
-echo " Installation de GNS3 terminé !"
+# ajout de groupes ci-dessous pour l'user en cours...
+sudo usermod -aG ubridge,libvirt,kvm,wireshark,docker $(whoami)
+echo "[7/7] Installation de GNS3 terminé !"
